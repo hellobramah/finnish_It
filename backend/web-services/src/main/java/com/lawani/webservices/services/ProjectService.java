6d3e8fe91f874b523396d977f714047264943148
Alex Bramah-Lawani <hellobramah@gmail.com>
2020-09-29 16:01:45 +0000

    Update ProjectService.java
    Fixed error caused by ommition of @Service

--- backend/web-services/src/main/java/com/lawani/webservices/services/ProjectService.java
+++ backend/web-services/src/main/java/com/lawani/webservices/services/ProjectService.java
@@ -1,17 +1,23 @@
 package com.lawani.webservices.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.lawani.webservices.domain.Project;
 import com.lawani.webservices.repositories.ProjectRepository;
 
+@Service
 public class ProjectService {
 
-	@Autowired
-	private ProjectRepository projectRepository;
+    @Autowired
+    private ProjectRepository projectRepository;
 
-	public Project saveOrUpdateProject(Project project) {
+    public Project saveOrUpdateProject(Project project){
 
-		return projectRepository.save(project);
-	}
+    
+
+        return projectRepository.save(project);
+    }
+
 }
+
