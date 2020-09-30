aa22da350ea9db898d10db1b03a550c39e05c88b
Alex Bramah-Lawani <hellobramah@gmail.com>
2020-09-30 15:05:49 +0000

    Revove Validation logic from Controller
    Let MapValidationErrorService handle validations instead

--- backend/web-services/src/main/java/com/lawani/webservices/web/ProjectController.java
+++ backend/web-services/src/main/java/com/lawani/webservices/web/ProjectController.java
@@ -1,22 +1,18 @@
 package com.lawani.webservices.web;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lawani.webservices.domain.Project;
+import com.lawani.webservices.services.MapValidationErrorService;
 import com.lawani.webservices.services.ProjectService;
 
 @RestController
@@ -25,17 +21,15 @@ public class ProjectController {
 
     @Autowired
     private ProjectService projectService;
+    
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
 
 
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult bindingResult){
-      if(bindingResult.hasErrors()) {
-    	  Map<String, String> errorMap = new HashMap<>();
-    	  for(FieldError error: bindingResult.getFieldErrors()) {
-    		  errorMap.put(error.getField(), error.getDefaultMessage());
-    	  }
-    	  return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
-      }
+     ResponseEntity<?> errorMap=mapValidationErrorService.MapValidationService(bindingResult);
+     if(errorMap!=null) {return errorMap;}
     	Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
