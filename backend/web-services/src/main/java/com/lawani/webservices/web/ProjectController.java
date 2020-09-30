248828c7cc5545239ffbf1a6f9c2bdde17b4c984
Alex Bramah-Lawani <hellobramah@gmail.com>
2020-09-30 00:40:45 +0000

    Update ProjectController.java
    Create Validation in Project Controller

--- backend/web-services/src/main/java/com/lawani/webservices/web/ProjectController.java
+++ backend/web-services/src/main/java/com/lawani/webservices/web/ProjectController.java
@@ -1,8 +1,16 @@
 package com.lawani.webservices.web;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +28,15 @@ public class ProjectController {
 
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
-        Project project1 = projectService.saveOrUpdateProject(project);
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult bindingResult){
+      if(bindingResult.hasErrors()) {
+    	  Map<String, String> errorMap = new HashMap<>();
+    	  for(FieldError error: bindingResult.getFieldErrors()) {
+    		  errorMap.put(error.getField(), error.getDefaultMessage());
+    	  }
+    	  return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+      }
+    	Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
 }
