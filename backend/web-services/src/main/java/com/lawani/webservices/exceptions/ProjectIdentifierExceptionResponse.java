42d01922efcf19faef6b7fc7c3bd9ce446d6c801
Alex Bramah-Lawani <hellobramah@gmail.com>
2020-10-05 04:11:58 +0000

    Create ProjectIdentifierExceptionResponse.java
    Create an exception response for  the Project Identifer exception

---
+++ backend/web-services/src/main/java/com/lawani/webservices/exceptions/ProjectIdentifierExceptionResponse.java
@@ -0,0 +1,22 @@
+package com.lawani.webservices.exceptions;
+
+public class ProjectIdentifierExceptionResponse {
+private String projectIdentifier;
+
+public ProjectIdentifierExceptionResponse(String projectIdentifier) {
+	
+	this.projectIdentifier = projectIdentifier;
+}
+
+public String getProjectIdentifier() {
+	return projectIdentifier;
+}
+
+public void setProjectIdentifier(String projectIdentifier) {
+	this.projectIdentifier = projectIdentifier;
+}
+
+
+
+
+}
