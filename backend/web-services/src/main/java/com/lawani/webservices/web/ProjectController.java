7095eed4666905a5ac89911f79da4ca70e7917fe
Alex Bramah-Lawani <hellobramah@gmail.com>
2020-10-09 02:41:48 +0000

    Create method in ProjectService to handle deletion of a project by ID
    Update ProjectController class to handle deletion of a method by ID

--- backend/web-services/src/main/java/com/lawani/webservices/web/ProjectController.java
+++ backend/web-services/src/main/java/com/lawani/webservices/web/ProjectController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,6 +49,14 @@ public class ProjectController {
 	public Iterable<Project> findAllProjects() {
 
 		return projectService.findAllProjects();
+
+	}
+	
+	@DeleteMapping("/{projectIdentifier}")
+	public ResponseEntity<?> deleteProject(@PathVariable String projectIdentifier) {
+		projectService.deleteProjectByIdentifier(projectIdentifier.toUpperCase());
+
+		return new ResponseEntity<String>("Project with ID '"+projectIdentifier.toUpperCase()+"' was deleted", HttpStatus.OK);
 
 	}
 }
